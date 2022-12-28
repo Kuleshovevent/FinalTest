c757cf06593965a5bbc75bf2f736dacb5e73e720
Anton <kuleshov.event@mail.ru>
2022-12-28 12:02:51 +0400

    Вводим новую функцию

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void ZadachaFinal()
+{
+    Console.Clear();
+    string[] ArrayStrings = new string[] { };
+    Console.Write("Введите количество элементов массива - строк:");
+    int n = int.Parse(Console.ReadLine());
+    ArrayStrings = new string[n];
+    for (int i = 0; i < ArrayStrings.Length; i++)
+    {
+        Console.Write($" Введите {i + 1} строку: ");
+        ArrayStrings[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("массив введенных данных:");
+    Console.WriteLine($"{PrintArray(ArrayStrings)}");
+    Console.WriteLine();
+
+    int length = 3;  // задаем парметр длины элемент массива
+
+    int numbers = CheckArray(ArrayStrings, length); 
+    // вводим новую переменную в которую будем присваивать вычесленный
+    // элемент массива меньше либо равна 3 символа
+
+ 
+}
+ZadachaFinal();
+
+string PrintArray(string [] array) // функция вывода массива
+{
+        string result = string.Empty;
+        result = "[ ";
+        for (int i = 0; i < array.Length; i++)
+        {
+            result += $"{array[i],1}";
+            if (i < array.Length - 1) result += ", ";
+        }
+        result += " ]";
+        return result;
+}
+
+// функция определения количества элемента массива длина которых меньше какой-то 
+//заданной длины length
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
